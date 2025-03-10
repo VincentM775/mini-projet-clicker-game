ec449051906e8fedd741cdef7e69a48d4090af7e
VincentM775 <vincent77.matias77@gmail.com>
2025-03-10 20:41:46 +0100

    push

--- lib/core/services/upgrade_service.dart
+++ lib/core/services/upgrade_service.dart
@@ -5,14 +5,11 @@ import '../services/api_service.dart';   // Service API pour effectuer les requ�
 class UpgradeService {
   final ApiService _apiService = ApiService(); // Instance de ApiService pour communiquer avec l'API
 
-  // Récupérer la liste des améliorations depuis l'API
-  Future<List<UpgradeModel>> getUpgrades() async {
+  // Récupérer la liste des améliorations depuis l'API pour un utilisateur donné
+  Future<List<UpgradeModel>> getUpgrades(int userId) async {
     try {
-      final data = await _apiService.getRequest('get_upgrades.php');  // Appel à l'API pour récupérer les améliorations
-      List<UpgradeModel> upgrades = [];  // Liste pour stocker les améliorations
-      for (var upgrade in data) {
-        upgrades.add(UpgradeModel.fromJson(upgrade));  // Mapper les données JSON dans des objets UpgradeModel
-      }
+      final data = await _apiService.getRequest('get_upgrades.php?user_id=$userId');
+      List<UpgradeModel> upgrades = data.map<UpgradeModel>((json) => UpgradeModel.fromJson(json)).toList();
       return upgrades;  // Retourner la liste des améliorations
     } catch (e) {
       print("Erreur lors de la récupération des améliorations: $e");
@@ -21,12 +18,18 @@ class UpgradeService {
   }
 
   // Appliquer une amélioration à l'utilisateur
-  Future<void> applyUpgrade(int userId, int upgradeId) async {
-    try {
-      final data = {'user_id': userId, 'upgrade_id': upgradeId};  // Paramètres pour l'amélioration
-      await _apiService.postRequest('apply_upgrade', data);  // Appel à l'API pour appliquer l'amélioration
-    } catch (e) {
-      throw Exception('Erreur lors de l\'application de l\'amélioration: $e');
-    }
+  Future<Map<String, dynamic>> applyUpgrade(int userId, int upgradeId) async {
+  try {
+    final data = {'user_id': userId, 'upgrade_id': upgradeId};
+    final response = await _apiService.postRequest('post_upgrade.php', data);
+
+    print("Réponse API applyUpgrade: $response"); // 🔍 Debug
+
+    return response;
+  } catch (e) {
+    print("Erreur lors de l'application de l'amélioration: $e");
+    return {'error': 'Une erreur est survenue'};
   }
+}
+
 }
