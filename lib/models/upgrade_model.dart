8a2112d5393254bb14c75d5c64d17c22e2dac555
VincentM775 <vincent77.matias77@gmail.com>
2025-03-11 01:02:43 +0100

    push clicker

--- lib/models/upgrade_model.dart
+++ lib/models/upgrade_model.dart
@@ -4,9 +4,9 @@ class UpgradeModel {
   final int id;
   final String name;
   final String description;
-  int cost;
-  int level;  // Ajout du niveau de l'amélioration
-  int costActual;
+  final int cost;
+  final int level;
+  final int costActual;
 
   UpgradeModel({
     required this.id,
@@ -14,17 +14,18 @@ class UpgradeModel {
     required this.description,
     required this.cost,
     required this.level,
-    required this.costActual
+    required this.costActual,
   });
 
   factory UpgradeModel.fromJson(Map<String, dynamic> json) {
+    int level = json['level'] ?? 1;
     return UpgradeModel(
       id: json['id'],
       name: json['name'],
       description: json['description'],
       cost: json['cost'],
-      level: json['level'] ?? 1,  // On récupère le niveau depuis l'API
-      costActual: json['cost'] * pow(2.1, json['level'] ?? 1).toInt(),
+      level: level,
+      costActual: (json['cost'] * pow(2.1, level)).round(),
     );
   }
 
@@ -35,7 +36,19 @@ class UpgradeModel {
       'description': description,
       'cost': cost,
       'level': level,
-      'costActual': costActual
+      'costActual': costActual,
     };
+  }
+
+  /// 🔹 Ajoute cette méthode pour créer une copie avec des valeurs modifiées
+  UpgradeModel copyWith({int? level, int? costActual}) {
+    return UpgradeModel(
+      id: id,
+      name: name,
+      description: description,
+      cost: cost,
+      level: level ?? this.level,
+      costActual: costActual ?? this.costActual,
+    );
   }
 }
